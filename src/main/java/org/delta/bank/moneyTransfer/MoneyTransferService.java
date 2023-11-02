96a724e54675898bdc5cc4e237364df4812d6b0f
Raberrse <ogelf1994@gmail.com>
2023-11-02 08:50:21 +0100

    Dependency Injection MoneyTransferService

--- src/main/java/org/delta/bank/moneyTransfer/MoneyTransferService.java
+++ src/main/java/org/delta/bank/moneyTransfer/MoneyTransferService.java
@@ -1,13 +1,14 @@
 package org.delta.bank.moneyTransfer;
 
+import com.google.inject.Inject;
 import org.delta.bank.account.BaseBankAccount;
 import org.delta.bank.feeCalculation.FeeCalculationService;
 import org.delta.bank.validation.ValidationService;
 
 public class MoneyTransferService {
 
-    ValidationService validationService = new ValidationService();
-    FeeCalculationService feeCalculationService = new FeeCalculationService();
+    @Inject ValidationService validationService = new ValidationService();
+    @Inject FeeCalculationService feeCalculationService = new FeeCalculationService();
 
     public void transferMoney(
             BaseBankAccount sourceAccount,
